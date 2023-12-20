dde50fbcde6b64b0b7fe8a76d675fba4eecb9bec
RogerRandomDev <rgrang816@west-mec.edu>
2023-12-19 23:32:22 -0700

    fixed errors from null values in the display scenes for variables in the editor

--- addons/settingGen/Editor/defaultCustomInputs/base.gd
+++ addons/settingGen/Editor/defaultCustomInputs/base.gd
@@ -7,7 +7,9 @@ extends HBoxContainer
 signal update_project_setting_value(new_value:Variant)
 var setting_resource
 
-func _ready():load_setting_data(setting_resource)
+func _ready():
+	if setting_resource!=null:
+		load_setting_data(setting_resource)
 
 func load_setting_data(data):
 	$Label.text=data.setting_name+" currently no preset display scene for %s. You can add you own by adding to the ProjectSetting class get_default_display_for_input function"%type_string(data.setting_type)
